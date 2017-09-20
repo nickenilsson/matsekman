895695180ad118ebb626a13c0765f7e389d0ccf2
nickenilsson <nilsson.sthlm@gmail.com>
2017-09-20 22:35:37 +0200

    History parsing in place

--- beerbot/matsekman.py
+++ beerbot/matsekman.py
@@ -23,59 +23,46 @@ cheers = [
 BOT_ID = os.environ.get("BOT_ID")
 
 # instantiate Slack & Twilio clients
-slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+#slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+slack_client = SlackClient("xoxb-169125561744-3g4L5lbhrBSLSfD5P5z0iRO4")
 
-beertab_file_path = "./beertab.json"
 
 FALLBACK_USERNAME = 'Jean Doe'
+SILENT_MODE = True
 
-try:
-    with open(beertab_file_path, 'r') as f:
-        beertabs = json.loads(f.read())
-except (ValueError, IOError):
-    beertabs = {}
-
-
-def storeBeerTab():
-    with open(beertab_file_path, 'w') as f:
-        f.write(json.dumps(beertabs))
-
+beertabs = {}
 
 def cleared(user_id, channel):
     username = beertabs[user_id].get('name', FALLBACK_USERNAME)
     beertabs[user_id]['balance'] = 0
     response = "Cleared! {}'s beer balance is 0 :beers:".format(username)
-    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
-    storeBeerTab()
-
+    if not SILENT_MODE:
+        slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
 def add_beer(user_id, amount, channel):
     username = beertabs[user_id].get('name', FALLBACK_USERNAME)
     beertabs[user_id]['balance'] = beertabs[user_id]['balance'] - amount
     sign = '+' if beertabs[user_id]['balance'] > 0 else ''
     response = "Thank you, {}! Your beer balance is *{}{}* :beers:".format(username, sign, beertabs[user_id]['balance'])
-    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
-    storeBeerTab()
-
+    if not SILENT_MODE:
+        slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
 def deduct_beer(user_id, amount, channel):
-
     username = beertabs[user_id].get('name', FALLBACK_USERNAME)
     beertabs[user_id]['balance'] = beertabs[user_id]['balance'] + amount
     sign = '+' if beertabs[user_id]['balance'] > 0 else ''
     response = "{}, {}! Your beer balance is *{}{}* :beers:".format(random.choice(cheers), username, sign,
                                                                     beertabs[user_id]['balance'])
-    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
-    storeBeerTab()
+    if not SILENT_MODE:
+        slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
 def set_balance(user_id, amount, channel):
     username = beertabs[user_id].get('name', FALLBACK_USERNAME)
     beertabs[user_id]['balance'] = amount
     sign = '+' if beertabs[user_id]['balance'] > 0 else ''
     response = "*{}*'s beer balance adjusted to *{}{}* :beers:".format(username, sign, amount)
-    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
-    storeBeerTab()
-
+    if not SILENT_MODE:
+        slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
 def show_help(channel):
     response = "Hello beer lovers! This is how you will speak to me:\n" \
@@ -86,13 +73,15 @@ def show_help(channel):
                "To zero out your balance, please say *cleared*\n" \
                "If you need to adjust your balance to x amount you can do that by saying *set x*\n" \
                "I am case insensitive. "
-    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+    if not SILENT_MODE:
+        slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
 def show_my_balance(user_id, channel):
     username = beertabs[user_id].get('name', FALLBACK_USERNAME)
     sign = '+' if beertabs[user_id]['balance'] > 0 else ''
     response = "{}'s current beer balance is *{}{}* :beers:".format(username, sign, beertabs[user_id]['balance'])
-    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+    if not SILENT_MODE:
+        slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
 def show_all_balances(channel):
     response = ""
@@ -101,7 +90,8 @@ def show_all_balances(channel):
             continue
         sign = '+' if item['balance'] > 0 else ''
         response += "*{}*: *{}{}* :beers:\n".format(item.get('name', FALLBACK_USERNAME), sign, item['balance'])
-    slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
+    if not SILENT_MODE:
+        slack_client.api_call("chat.postMessage", channel=channel, text=response, as_user=True)
 
 
 
@@ -115,12 +105,6 @@ def handle_command(command, channel):
     if not beertabs[user_id].get('name'):
         username = slack_client.api_call("users.info", user=user_id).get('user', {}).get('name')
         beertabs[user_id]['name'] = username
-
-
-    if re.search(r"(?i)^puppet:(.+)", command['text']) and user_id == 'U4YUU5YSG':
-        response = re.search(r"(?i)^puppet:(.+)", command['text']).group(1)
-        slack_client.api_call("chat.postMessage", channel='C4YRPHYV9', text=response, as_user=True)
-        return
 
     elif command['text'].lower().strip() == 'cleared':
         cleared(user_id=user_id, channel=channel)
@@ -156,6 +140,7 @@ def handle_command(command, channel):
         set_balance(user_id=user_id, amount=amount, channel=channel)
         return
 
+
 def parse_slack_output(slack_rtm_output):
 
     output_list = slack_rtm_output
@@ -168,11 +153,77 @@ def parse_slack_output(slack_rtm_output):
     return None, None
 
 
+def get_member_channels():
+    cursor = None
+    channel_ids = []
+    while True:
+        response = slack_client.api_call(
+            "channels.list",
+            cursor=cursor
+        )
+        for c in response["channels"]:
+            if c["is_member"] == False:
+                continue
+            else:
+                channel_ids.append(c["id"])
+        if response["response_metadata"].get("next_cursor"):
+            cursor = response["response_metadata"].get("next_cursor")
+        else:
+            break
+    return channel_ids
+
+def find_channel_id(name):
+    cursor=None
+    while True:
+        params = {"method": "channels.list"}
+        if cursor:
+            params["cursor"] = cursor
+        response = slack_client.api_call(**params)
+
+        for c in response["channels"]:
+            if c["name"].lower() == name.lower():
+                return c["id"]
+        if response.get("response_metadata", {}).get("next_cursor"):
+            cursor = response["response_metadata"]["next_cursor"]
+        else:
+            break
+
+
+def get_balance_from_channel(channel_id):
+    last_message_id = None
+    all_messages = []
+    while True:
+        params = {"method": "channels.history", "channel": channel_id}
+        if last_message_id:
+            params["latest"] = last_message_id
+        response = slack_client.api_call(**params)
+
+        for message in response["messages"]:
+            all_messages.append(message)
+        if not response["has_more"]:
+            break
+
+        last_message_id = response["messages"][-1]["ts"]
+
+    print len(all_messages)
+    for i in range(len(all_messages)-1, 0, -1):
+        handle_command(all_messages[i], channel_id)
+
+def initilize_beer_balance():
+    beer_channel_id = find_channel_id("beer_balance")
+    get_balance_from_channel(beer_channel_id)
+
+
 if __name__ == "__main__":
 
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
         print("StarterBot connected and running!")
+        print "initializing beer balances..."
+        initilize_beer_balance()
+        print "Beer balances are initialized!"
+        SILENT_MODE = False
+
         while True:
             try:
                 command, channel = parse_slack_output(slack_client.rtm_read())
